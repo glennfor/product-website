b62c55dede7f730972ff6ddf0ba855af4c2fcff2
JIM440 <takemjim43@gmail.com>
2023-11-26 01:30:26 +0100

    more changes

--- index.js
+++ index.js
@@ -1,80 +1,82 @@
-// replace big image with small images when clicked
-const bigImages = document.querySelectorAll('.large-image-container > img')
-const smallImages = document.querySelectorAll('.small-image > img')
+// replace big image src when small image is clicked
+const bigImages = $('.large-image-container > img');
+const smallImages = $('.small-image > img');
 
-smallImages.forEach(smallImage => {
-    smallImage.addEventListener('click', ()=>{
-        removeActiveClassParent(smallImages, 'active')
-        removeActiveClass(bigImages, 'visible')
-        smallImage.parentElement.classList.add('active')
+smallImages.each((index, smallImage) => {
+  $(smallImage).on('click', () => {
+    removeActiveClassParent(smallImages, 'active');
+    removeActiveClass(bigImages, 'visible');
+    $(smallImage).parent().addClass('active');
 
-        bigImages.forEach(bigImage => {
-            if(bigImage.src == smallImage.src){
-                bigImage.classList.add('visible')
-            }
-        })
-    })
-})
-// // full size image
-// const fullscreen = document.querySelector('.expand')
-// fullscreen.addEventListener('click', () => {
-    
-// })
+    bigImages.each((index, bigImage) => {
+      if ($(bigImage).attr('src') == $(smallImage).attr('src')) {
+        $(bigImage).addClass('visible');
+      }
+    });
+  });
+});
 
+// wishlist
+const addWishlist = $('.add-wishlist');
+
+addWishlist.on('click', () => {
+  addWishlist.toggleClass('active');
+});
 
 // tabs for product page
-const tabBtns = document.querySelectorAll('.tab-btn')
-const tabContents = document.querySelectorAll('.tab-content')
+const tabBtns = $('.tab-btn');
+const tabContents = $('.tab-content');
 
-tabBtns.forEach(tabBtn => {
-    tabBtn.addEventListener('click', () => {
-         removeActiveClass(tabBtns, 'active')
-         removeActiveClass(tabContents, 'active')
-        tabBtn.classList.add('active')
-        document.querySelector(tabBtn.dataset.tabTarget).classList.add('active')
-        })
-})
+tabBtns.each((index, tabBtn) => {
+  $(tabBtn).on('click', () => {
+    removeActiveClass(tabBtns, 'active');
+    removeActiveClass(tabContents, 'active');
+    $(tabBtn).addClass('active');
+    $($(tabBtn).data('tabTarget')).addClass('active');
+  });
+});
 
 // rating, updated star color when clicked
-const stars = document.querySelectorAll('.add-review-form .stars > i')
-// index of clicked star
-stars.forEach((star, index) => {
-    star.addEventListener('click', () => {
-        // remove or add active to other stars
-        stars.forEach((preStar, preIdx) => {
-            if(preIdx <= index){
-                preStar.classList.add('active')
-            }
-            else{
-                preStar.classList.remove('active')
-            }
-        })
-    })
-})
+const stars = $('.add-review-form .stars > i');
+
+stars.each((index, star) => {
+  $(star).on('click', () => {
+    stars.each((preIdx, preStar) => {
+      if (preIdx <= index) {
+        $(preStar).addClass('active');
+      } else {
+        $(preStar).removeClass('active');
+      }
+    });
+  });
+});
+
 // add reviews
-const reviewContainer = document.querySelector('.add-review-container')
+const reviewContainer = $('.add-review-container');
+const textareaReview = $('.add-review-form textarea');
+const addReview = $('.add-review');
+const closeReview = $('.close-review');
 
-const addReview = document.querySelector('.add-review')
+addReview.on('click', () => {
+  reviewContainer.addClass('active');
+});
 
-const closeReview = document.querySelector('.close-review')
-
-addReview.addEventListener('click', () =>{
-    reviewContainer.classList.add('active')
-})
-closeReview.addEventListener('click', () =>{
-    reviewContainer.classList.remove('active')
-    removeActiveClass(stars, 'active')
-})
+closeReview.on('click', () => {
+  reviewContainer.removeClass('active');
+  removeActiveClass(stars, 'active');
+  textareaReview.val('');
+});
 
 // general functions
 // remove active class
-const removeActiveClassParent = (items, className)=>{
-    items.forEach(item => {
-        item.parentElement.classList.remove(className)
-    });
-}
+const removeActiveClassParent = (items, className) => {
+  items.each((index, item) => {
+    $(item).parent().removeClass(className);
+  });
+};
+
 const removeActiveClass = (items, className) => {
-    items.forEach(item => {
-        item.classList.remove(className)
-    });
-}
+  items.each((index, item) => {
+    $(item).removeClass(className);
+  });
+};
