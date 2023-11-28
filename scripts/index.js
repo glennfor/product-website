25ae68c42152200e8298a744e476958120ca65a0
JIM440 <takemjim43@gmail.com>
2023-11-28 21:57:02 +0100

    compress and resize photos

---
+++ scripts/index.js
@@ -0,0 +1,96 @@
+// replace big image src when small image is clicked
+const bigImages = $('.large-image-container > img');
+const smallImages = $('.small-image > img');
+
+smallImages.each((index, smallImage) => {
+  $(smallImage).on('click', () => {
+    removeActiveClassParent(smallImages, 'active');
+    removeActiveClass(bigImages, 'visible');
+    $(smallImage).parent().addClass('active');
+
+    bigImages.each((index, bigImage) => {
+      if ($(bigImage).attr('src') == $(smallImage).attr('src')) {
+        $(bigImage).addClass('visible');
+      }
+    });
+  });
+});
+
+// wishlist
+const addWishlist = $('.add-wishlist');
+
+addWishlist.on('click', () => {
+  addWishlist.toggleClass('active');
+});
+
+// tabs for product page
+const tabBtns = $('.tab-btn');
+const tabContents = $('.tab-content');
+
+tabBtns.each((index, tabBtn) => {
+  $(tabBtn).on('click', () => {
+    removeActiveClass(tabBtns, 'active');
+    removeActiveClass(tabContents, 'active');
+    $(tabBtn).addClass('active');
+    $($(tabBtn).data('tabTarget')).addClass('active');
+  });
+});
+
+// rating, updated star color when clicked
+const stars = $('.add-review-form .stars > i');
+
+stars.each((index, star) => {
+  $(star).on('click', () => {
+    stars.each((preIdx, preStar) => {
+      if (preIdx <= index) {
+        $(preStar).addClass('active');
+      } else {
+        $(preStar).removeClass('active');
+      }
+    });
+  });
+});
+
+// add reviews
+const reviewContainer = $('.add-review-container');
+const textareaReview = $('.add-review-form textarea');
+const showReviewContainer = $('.add-review');
+const closeReviewContainer = $('.close-review');
+
+showReviewContainer.on('click', () => {
+  reviewContainer.addClass('active');
+});
+
+closeReviewContainer.on('click', () => {
+  reviewContainer.removeClass('active');
+  removeActiveClass(stars, 'active');
+  textareaReview.val('');
+});
+//
+// add review element
+// 
+
+
+
+// const ratingReviewContainer = $('.rating-review-container')
+
+
+
+
+
+
+
+
+// general functions
+// remove active class
+const removeActiveClassParent = (items, className) => {
+  items.each((index, item) => {
+    $(item).parent().removeClass(className);
+  });
+};
+
+const removeActiveClass = (items, className) => {
+  items.each((index, item) => {
+    $(item).removeClass(className);
+  });
+};
